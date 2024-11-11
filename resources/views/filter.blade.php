03489f64ac6754ca1ef6655c99c2a5dbd275488e
yesiming <34021144+yesiming@users.noreply.github.com>
2024-11-11 10:45:32 +0800

    Update filter.blade.php
    改为过滤name

--- resources/views/filter.blade.php
+++ resources/views/filter.blade.php
@@ -1,5 +1,5 @@
 <div class="filter-input">
-    <div class="input-group input-group-sm" id="{{ $id }}" data-value-type="code">
+    <div class="input-group input-group-sm" id="{{ $id }}" data-value-type="name">
         <div class="input-group-prepend">
             <span class="input-group-text bg-white text-capitalize"  data-toggle="tooltip" data-placement="top"><b>{!! $label !!}</b></span>
         </div>
